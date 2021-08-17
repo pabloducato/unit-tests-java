61a4c1d99cdcdb5e77d6e18da8256c5550b43d0c
Paweł Kocan <pkocan@onet.pl>
2021-08-17 22:02:14 +0200

    11. Test structure (pol. Struktura testu) -> behaviour driven development (BDD)

--- src/test/java/pl/devfoundry/testing/AccountTest.java
+++ src/test/java/pl/devfoundry/testing/AccountTest.java
@@ -9,27 +9,38 @@ class AccountTest {
 
     @Test
     void myTest1() {
+        //given
+        //when
         final Account account = new Account();
+        //then
         assertFalse(account.isActive());
     }
 
     @Test
     void myTest2() {
+        //given
         final Account account = new Account();
+        //when
         account.activate();
+        //then
         assertTrue(account.isActive());
     }
 
     @Test
     void myTest3() {
+        //given
+        //when
         final Account account = new Account();
         assertFalse(account.isActive(), "Check if new account is not active.");
     }
 
     @Test
     void myTest4() {
+        //given
         final Account account = new Account();
+        //when
         account.activate();
+        //then
         assertTrue(account.isActive(), "Check if new account was set to active.");
     }
 
